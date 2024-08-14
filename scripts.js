60202dfda2b4b70698c8ea706d0f24535b6a3216
Connor Lance <connorlance02@gmail.com>
2024-08-14 15:47:35 -0400

    Completed responsive navbar

---
+++ scripts.js
@@ -0,0 +1,17 @@
+/* ################################### */
+/* #              NAVBAR             # */
+/* ################################### */
+const toggleButton = document.querySelector(".toggle-button");
+const navbarLinks = document.querySelector(".navbar-links");
+
+// Toggle the navbar and toggle button styles on button click
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+  toggleButton.classList.toggle("active");
+});
+
+// Close the navbar when any link inside navbar-links is clicked
+navbarLinks.addEventListener("click", () => {
+  navbarLinks.classList.remove("active");
+  toggleButton.classList.remove("active");
+});
